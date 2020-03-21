4f4440ab08be564cc01b19f5d1740842e8ecb3f4
Vitalii <vitaliybondarenko1982@gmail.com>
2020-03-21 15:08:12 +0200

    Initial dist subtree commit

---
+++ precache-manifest.be1d02f5add41ce7deb705944f0ac746.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb876eedec204283e869",
+    "url": "/vue-wallet-project/css/app.4e8e286f.css"
+  },
+  {
+    "revision": "07c107c89e5ea4547ef9",
+    "url": "/vue-wallet-project/css/chunk-vendors.2955f2db.css"
+  },
+  {
+    "revision": "6fa899fca33bb17f324569efb9b92f5b",
+    "url": "/vue-wallet-project/index.html"
+  },
+  {
+    "revision": "eb876eedec204283e869",
+    "url": "/vue-wallet-project/js/app.0459f076.js"
+  },
+  {
+    "revision": "578f143b59a2b131db76",
+    "url": "/vue-wallet-project/js/chunk-2d0c1056.7a76ecb6.js"
+  },
+  {
+    "revision": "a31b3617438d84ed0c33",
+    "url": "/vue-wallet-project/js/chunk-2d0c9708.65c2e6bd.js"
+  },
+  {
+    "revision": "638c99a67d563a5f3ddb",
+    "url": "/vue-wallet-project/js/chunk-2d0d05dc.17b09f0d.js"
+  },
+  {
+    "revision": "49b6fa2b744cda2d076a",
+    "url": "/vue-wallet-project/js/chunk-2d217357.7c8f1645.js"
+  },
+  {
+    "revision": "59eeb9a83c8bea1f329c",
+    "url": "/vue-wallet-project/js/chunk-2d217a99.1ae90684.js"
+  },
+  {
+    "revision": "7689c28cf630f47fad6f",
+    "url": "/vue-wallet-project/js/chunk-2d21a3d2.88e7e21f.js"
+  },
+  {
+    "revision": "ae56969265faf98cbed6",
+    "url": "/vue-wallet-project/js/chunk-2d2259e5.0b959f4d.js"
+  },
+  {
+    "revision": "57f2ad5e22101410c94d",
+    "url": "/vue-wallet-project/js/chunk-37f41dab.576d5a33.js"
+  },
+  {
+    "revision": "51b22299313ad6cb60d6",
+    "url": "/vue-wallet-project/js/chunk-3fa92a6f.151af61a.js"
+  },
+  {
+    "revision": "07c107c89e5ea4547ef9",
+    "url": "/vue-wallet-project/js/chunk-vendors.7bdceb94.js"
+  },
+  {
+    "revision": "8f2716d0ba713170fdec0ca921d4d8d5",
+    "url": "/vue-wallet-project/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-wallet-project/robots.txt"
+  }
+]);
